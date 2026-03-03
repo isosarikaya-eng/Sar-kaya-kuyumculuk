2114df1404709fec3e4a5249315242fdfe7cf092
isosarikaya-eng <iso.sarikaya@gmail.com>
2026-03-03 14:40:33 +0300

    initial clean start

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Sarrafiye TV",
+    page_icon="💎",
+    layout="wide"
+)
+
+st.title("💎 Sarrafiye TV")
+
+st.markdown("---")
+
+st.subheader("Sistem Hazır")
+
+st.success("Yeni sistem başarıyla çalışıyor.")
